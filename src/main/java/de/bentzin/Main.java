8a0aa5cb3a98bdb1197ba3e7e91171efc6ccf65d
TDR <tdr.minecraft@gmail.com>
2023-10-07 14:35:24 +0200

    initial commit

---
+++ src/main/java/de/bentzin/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin;
+
+
+/**
+ * @author Ture Bentzin
+ * @since 07-10-2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
